317d79fe10b866df9ed0c9b16c50043dbf276316
Safa Bettaieb <safa.bettaieb1@gmail.com>
2020-02-27 00:18:22 +0100

    create TestTemperatureConsume class and add testConsumeMax method

--- src/main/java/com/safa/temperature/soap/api/controllers/TestTemperatureConsume.java
+++ src/main/java/com/safa/temperature/soap/api/controllers/TestTemperatureConsume.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.safa.temperature.soap.api.services.TemperatureService;
+import com.safa.temperature.soap.api.temperatureRequestResponse.MaxTemperatureRequest;
+import com.safa.temperature.soap.api.temperatureRequestResponse.MaxTemperatureResponse;
 import com.safa.temperature.soap.api.temperatureRequestResponse.MinTemperatureRequest;
 import com.safa.temperature.soap.api.temperatureRequestResponse.MinTemperatureResponse;
 
@@ -20,6 +22,10 @@ public class TestTemperatureConsume {
 	@PostMapping("/min")
 	public MinTemperatureResponse testConsumeMin(@RequestBody MinTemperatureRequest request) {
 		return service.getMinTemperature(request);
+	}
+	@PostMapping("/max")
+	public MaxTemperatureResponse testConsumeMax(@RequestBody MaxTemperatureRequest request) {
+		return service.getMaxTemperature(request);
 	}
 
 }
