1768f2b88dcb28a1104c90cdbfc5a8571db040a9
Safa Bettaieb <safa.bettaieb1@gmail.com>
2020-02-27 00:29:44 +0100

    add testConsumeClosestToZero method in TestTemperatureConsume

--- src/main/java/com/safa/temperature/soap/api/controllers/TestTemperatureConsume.java
+++ src/main/java/com/safa/temperature/soap/api/controllers/TestTemperatureConsume.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.safa.temperature.soap.api.services.TemperatureService;
 import com.safa.temperature.soap.api.temperatureRequestResponse.AvgTemperatureRequest;
 import com.safa.temperature.soap.api.temperatureRequestResponse.AvgTemperatureResponse;
+import com.safa.temperature.soap.api.temperatureRequestResponse.ClosestToZeroTemperatureRequest;
+import com.safa.temperature.soap.api.temperatureRequestResponse.ClosestToZeroTemperatureResponse;
 import com.safa.temperature.soap.api.temperatureRequestResponse.MaxTemperatureRequest;
 import com.safa.temperature.soap.api.temperatureRequestResponse.MaxTemperatureResponse;
 import com.safa.temperature.soap.api.temperatureRequestResponse.MinTemperatureRequest;
@@ -19,19 +21,24 @@ import com.safa.temperature.soap.api.temperatureRequestResponse.MinTemperatureRe
 public class TestTemperatureConsume {
 	@Autowired
 	TemperatureService service;
-	
-	
+
 	@PostMapping("/min")
 	public MinTemperatureResponse testConsumeMin(@RequestBody MinTemperatureRequest request) {
 		return service.getMinTemperature(request);
 	}
+
 	@PostMapping("/max")
 	public MaxTemperatureResponse testConsumeMax(@RequestBody MaxTemperatureRequest request) {
 		return service.getMaxTemperature(request);
 	}
+
 	@PostMapping("/avg")
 	public AvgTemperatureResponse testConsumeAvg(@RequestBody AvgTemperatureRequest request) {
 		return service.getAvgTemperature(request);
 	}
 
+	@PostMapping("/ClosestToZero")
+	public ClosestToZeroTemperatureResponse testConsumeClosestToZero(@RequestBody ClosestToZeroTemperatureRequest request) {
+		return service.getClosestToZeroTemperature(request);
+	}
 }
