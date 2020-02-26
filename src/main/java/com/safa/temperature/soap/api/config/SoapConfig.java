ec01eac695ded71c2575c3c08aa3b13e933c2b61
Safa Bettaieb <safa.bettaieb1@gmail.com>
2020-02-26 23:33:45 +0100

    create SoapConfig class and add marshaller method

---
+++ src/main/java/com/safa/temperature/soap/api/config/SoapConfig.java
@@ -0,0 +1,16 @@
+package com.safa.temperature.soap.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+public class SoapConfig {
+	@Bean
+	public Jaxb2Marshaller marshaller() {
+		Jaxb2Marshaller marshaller =new Jaxb2Marshaller();
+		marshaller.setPackagesToScan("com.safa.loan.soap.api.temperatureRequestResponse");
+		return marshaller;
+	}
+
+}
